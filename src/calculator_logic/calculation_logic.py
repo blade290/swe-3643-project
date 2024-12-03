ee4bc7a8a266d339455e11bbd0151c1a1fc7403e
Joss Hufnagel <43077216+blade290@users.noreply.github.com>
2024-12-02 20:52:49 -0500

    Calculator logic and tests

---
+++ src/calculator_logic/calculation_logic.py
@@ -0,0 +1,177 @@
+import re
+
+from src.calculator_logic.calculation_result import CalculationResult
+import math
+
+
+def is_float(string):
+    pattern = r"^-?\d+(\.\d+)?$"
+    match = re.match(pattern, string)
+    return bool(match)
+
+def calculate_mean(num_list):
+    # preq-LOGIC-5
+    try:
+        num_list = num_list.split('\n')
+        for num in num_list:
+            num.strip()
+        while '' in num_list:
+            num_list.remove('')
+        if not num_list:
+            raise ValueError("No numbers detected. One value per line.")
+        for num in num_list:
+            if len(num.split(' ')) > 1:
+                raise ValueError("Multiple values detected. One value per line.")
+            if not is_float(num):
+                raise ValueError("Non-numeric value detected. One value per line.")
+        for i in range(len(num_list)):
+            num_list[i] = float(num_list[i])
+        mean = sum(num_list) / len(num_list)
+        return CalculationResult(mean, True, "Mean", "")
+    except ValueError as e:
+        return CalculationResult(0.0, False, "", e)
+
+
+def calculate_sample_standard_deviation(num_list):
+    #preq-LOGIC-3
+    try:
+        num_list = num_list.split('\n')
+        for num in num_list:
+            num.strip()
+        while '' in num_list:
+            num_list.remove('')
+        if not num_list:
+            raise ValueError("No numbers detected. One value per line.")
+        for num in num_list:
+            if len(num.split(' ')) > 1:
+                raise ValueError("Multiple values detected. One value per line.")
+            if not is_float(num):
+                raise ValueError("Non-numeric value detected. One value per line.")
+        for i in range(len(num_list)):
+            num_list[i] = float(num_list[i])
+        mean = sum(num_list) / len(num_list)
+        sample_variance = sum(pow((i - mean), 2) for i in num_list) / (len(num_list)-1)
+        sample_standard_deviation = math.sqrt(sample_variance)
+        return CalculationResult(sample_standard_deviation, True, "Standard Deviation", "")
+    except ValueError as e:
+        return CalculationResult(0.0, False, "", e)
+
+
+def calculate_population_standard_deviation(num_list):
+    #preq-LOGIC-4
+    try:
+        num_list = num_list.split('\n')
+        for num in num_list:
+            num.strip()
+        while '' in num_list:
+            num_list.remove('')
+        if not num_list:
+            raise ValueError("No numbers detected. One value per line.")
+        for num in num_list:
+            if len(num.split(' ')) > 1:
+                raise ValueError("Multiple values detected. One value per line.")
+            if not is_float(num):
+                raise ValueError("Non-numeric value detected. One value per line.")
+        if len(num_list) == 1:
+            raise ValueError("One number detected. Two or more numbers required.")
+        for i in range(len(num_list)):
+            num_list[i] = float(num_list[i])
+        mean = sum(num_list) / len(num_list)
+        population_variance = sum(pow((i - mean), 2) for i in num_list) / len(num_list)
+        population_standard_deviation = math.sqrt(population_variance)
+        return CalculationResult(population_standard_deviation, True, "Population Standard Deviation", "")
+    except ValueError as e:
+        return CalculationResult(0.0, False, "", e)
+
+def calculate_zscore(input_list):
+    #preq-LOGIC-6
+    try:
+        input_list = input_list.split('\n')
+        while '' in input_list:
+            input_list.remove('')
+        if len(input_list) == 0:
+            raise ValueError("No Values detected. Format is \"value,mean,standard_deviation\" on one line.")
+        elif len(input_list) > 1:
+            raise ValueError("Multiple lines detected. Format is \"value,mean,standard_deviation\" on one line.")
+        input_list = input_list[0].split(',')
+        while '' in input_list:
+            input_list.remove('')
+        for i in range(len(input_list)):
+            input_list[i] = input_list[i].strip()
+            check = input_list[i].split(' ')
+            if len(check) > 1:
+                raise ValueError("Multiple values detected. Format is \"value,mean,standard_deviation\" on one line.")
+            if not is_float(input_list[i]):
+                raise ValueError("Non-numeric value detected. Format is \"value,mean,standard_deviation\" on one line.")
+        if not len(input_list) == 3:
+            raise ValueError("Format is \"value,mean,standard_deviation\" on one line.")
+        for i in range(len(input_list)):
+            input_list[i] = float(input_list[i])
+        value = input_list[0]
+        mean = input_list[1]
+        standard_deviation = input_list[2]
+        if standard_deviation == 0:
+            raise ValueError("Division by Zero. Standard Deviation cannot be zero.")
+        zscore = (value - mean) / standard_deviation
+        return CalculationResult(zscore, True, "ZScore", "")
+    except ValueError as e:
+        return CalculationResult(0.0, False, "", e)
+
+def calculate_single_linear_regression(input_list):
+    #preq-LOGIC-7
+    try:
+        input_list = input_list.split('\n')
+        while '' in input_list:
+            input_list.remove('')
+        if len(input_list) == 0:
+            raise ValueError("Empty list. Format is one \"x,y\" pair per line.")
+        for i in range(len(input_list)):
+            input_list[i] = input_list[i].strip()
+            if not re.match(r"^-?\d+(?:\.\d+)?, ?-?\d+(?:\.\d+)?$", input_list[i]):
+                raise ValueError("Invalid Format. Format is one \"x,y\" pair per line.")
+        x_values = []
+        y_values = []
+        for xypair in input_list:
+            pair = xypair.split(',')
+            x_values.append(float(pair[0]))
+            y_values.append(float(pair[1]))
+        if len(input_list) == 1:
+            raise ValueError("One pair detected. Multiple pairs needed. Format is one \"x,y\" pair per line.")
+        numerator = 0
+        denominator = 0
+        x_mean = sum(x_values) / len(x_values)
+        y_mean = sum(y_values) / len(y_values)
+        for x,y in zip(x_values, y_values):
+            numerator += (x - x_mean) * (y - y_mean)
+            denominator += (x - x_mean) ** 2
+        slope = numerator / denominator
+        intercept = y_mean - slope * x_mean
+        equation = f'y = {round(slope,10)}x + {round(intercept,10)}'
+        return CalculationResult(equation, True, "Single Linear Regression", "")
+    except ValueError as e:
+        return CalculationResult(0.0, False, "", e)
+
+def calculate_y_prediction(input_list):
+    #preq-LOGIC-8
+    try:
+        input_list = input_list.split('\n')
+        while '' in input_list:
+            input_list.remove('')
+        if len(input_list) == 0:
+            raise ValueError("Empty input. Format is \"x, m, b\" on one line.")
+        if len(input_list) > 1:
+            raise ValueError("Multiple lines detected. Format is \"x, m, b\" on one line.")
+        input_list = input_list[0].split(',')
+        for i in range(len(input_list)):
+            input_list[i] = input_list[i].strip()
+            if not re.match(r"^-?\d+(?:\.\d+)?$", input_list[i]):
+                raise ValueError("Non-Numbers detected. Format is \"x, m, b\" on one line.")
+        if len(input_list) != 3:
+            raise ValueError("Missing Value(s). Format is \"x, m, b\" on one line.")
+        for i in range(len(input_list)):
+            input_list[i] = float(input_list[i])
+        y_value = (input_list[0] * input_list[1]) + input_list[2]
+        result = f'y = {y_value}'
+        return CalculationResult(result, True, "Y Prediction", "")
+    except ValueError as e:
+        return CalculationResult(0.0, False, "", e)
---
+++ src/tests/calculator_logic_tests.py
@@ -0,0 +1,227 @@
+from src.calculator_logic.calculation_logic import *
+
+
+def test_CalculateMean_ValidList_ReturnsMean():
+    #preq-UNIT-TEST-4
+    input_list = "9\n6\n8\n5\n7"
+    expected = 7.0
+    actual = calculate_mean(input_list).result
+    assert actual == expected
+
+
+def test_CalculateMean_EmptyList_ReturnsError():
+    #preq-UNIT-TEST-4
+    input_list = ""
+    expected = 'No numbers detected. One value per line.'
+    actual = str(calculate_mean(input_list).error)
+    assert actual == expected
+
+
+def test_CalculateMean_MultipleValues_ReturnsError():
+    #preq-UNIT-TEST-4
+    input_list = "9 6\n3 4\n66"
+    expected = 'Multiple values detected. One value per line.'
+    actual = str(calculate_mean(input_list).error)
+    assert actual == expected
+
+
+def test_CalculateMean_NonNumeric_ReturnsError():
+    #preq-UNIT-TEST-4
+    input_list = "96\n34b\n66"
+    expected = 'Non-numeric value detected. One value per line.'
+    actual = str(calculate_mean(input_list).error)
+    assert actual == expected
+
+
+def test_SampleStandardDeviation_ValidList_ReturnsStandardDeviation():
+    #preq-UNIT-TEST-2
+    input_list = "9\n6\n8\n5\n7\n"
+    expected = 1.5811388300841898
+    actual = calculate_sample_standard_deviation(input_list).result
+    assert actual == expected
+
+
+def test_SampleStandardDeviation_EmptyList_ReturnsError():
+    # preq-UNIT-TEST-2
+    input_list = ""
+    expected = 'No numbers detected. One value per line.'
+    actual = str(calculate_sample_standard_deviation(input_list).error)
+    assert actual == expected
+
+
+def test_SampleStandardDeviation_NonNumeric_ReturnsError():
+    # preq-UNIT-TEST-2
+    input_list = "96\n34b\n66"
+    expected = 'Non-numeric value detected. One value per line.'
+    actual = str(calculate_sample_standard_deviation(input_list).error)
+    assert actual == expected
+
+
+def test_SampleStandardDeviation_MultipleValues_ReturnsError():
+    # preq-UNIT-TEST-2
+    input_list = "9 6\n3 4\n66"
+    expected = 'Multiple values detected. One value per line.'
+    actual = str(calculate_sample_standard_deviation(input_list).error)
+    assert actual == expected
+
+def test_PopulationStandardDeviation_ValidList_ReturnsPopulationStandardDeviation():
+    # preq-UNIT-TEST-3
+    input_list = "9\n6\n8\n5\n7\n"
+    expected = 1.4142135623730951
+    actual = calculate_population_standard_deviation(input_list).result
+    assert actual == expected
+
+
+def test_PopulationStandardDeviation_EmptyList_ReturnsError():
+    # preq-UNIT-TEST-3
+    input_list = ""
+    expected = 'No numbers detected. One value per line.'
+    actual = str(calculate_population_standard_deviation(input_list).error)
+    assert actual == expected
+
+def test_PopulationStandardDeviation_NonNumeric_ReturnsError():
+    # preq-UNIT-TEST-3
+    input_list = "96\n34b\n66"
+    expected = 'Non-numeric value detected. One value per line.'
+    actual = str(calculate_population_standard_deviation(input_list).error)
+    assert actual == expected
+
+def test_PopulationStandardDeviation_MultipleValues_ReturnsError():
+    # preq-UNIT-TEST-3
+    input_list = "9 6\n3 4\n66"
+    expected = 'Multiple values detected. One value per line.'
+    actual = str(calculate_population_standard_deviation(input_list).error)
+    assert actual == expected
+
+def test_PopulationStandardDeviation_OneValue_ReturnsError():
+    # preq-UNIT-TEST-3
+    input_list = "9"
+    expected = 'One number detected. Two or more numbers required.'
+    actual = str(calculate_population_standard_deviation(input_list).error)
+    assert actual == expected
+
+def test_ZScore_ValidInput_ReturnsZScore():
+    #preq-UNIT-TEST-5
+    input_list = "11.5,7,1.5811388300841898"
+    expected = 2.846049894151541
+    actual = calculate_zscore(input_list).result
+    assert actual == expected
+
+def test_ZScore_EmptyInput_ReturnsError():
+    #preq-UNIT-TEST-5
+    input_list = ""
+    expected = 'No Values detected. Format is \"value,mean,standard_deviation\" on one line.'
+    actual = str(calculate_zscore(input_list).error)
+    assert actual == expected
+
+def test_Zscore_MissingParameters_ReturnsError():
+    # preq-UNIT-TEST-5
+    input_list = "9,3,"
+    expected = 'Format is \"value,mean,standard_deviation\" on one line.'
+    actual = str(calculate_zscore(input_list).error)
+    assert actual == expected
+
+def test_ZScore_MultipleLines_ReturnsError():
+    # preq-UNIT-TEST-5
+    input_list = "33,\n34,\n1"
+    expected = 'Multiple lines detected. Format is \"value,mean,standard_deviation\" on one line.'
+    actual = str(calculate_zscore(input_list).error)
+    assert actual == expected
+
+def test_ZScore_NonNumeric_ReturnsError():
+    # preq-UNIT-TEST-5
+    input_list = "9,34b,66"
+    expected = 'Non-numeric value detected. Format is \"value,mean,standard_deviation\" on one line.'
+    actual = str(calculate_zscore(input_list).error)
+    assert actual == expected
+
+def test_ZScore_MultipleValues_ReturnsError():
+    # preq-UNIT-TEST-5
+    input_list = "33 4, 32, 2"
+    expected = 'Multiple values detected. Format is \"value,mean,standard_deviation\" on one line.'
+    actual = str(calculate_zscore(input_list).error)
+    assert actual == expected
+
+def test_ZScore_StandardDeviationZero_ReturnsError():
+    # preq-UNIT-TEST-5
+    input_list = "33, 34, 0"
+    expected = 'Division by Zero. Standard Deviation cannot be zero.'
+    actual = str(calculate_zscore(input_list).error)
+    assert actual == expected
+
+def test_LinearRegression_ValidInput_ReturnsFormula():
+    #preq-UNIT-TEST-6
+    input_list = "1.47,52.21\n\n1.5,53.12\n\n1.52,54.48\n\n1.55,55.84\n\n1.57,57.2\n\n1.6,58.57\n\n1.63,59.93\n\n1.65,61.29\n\n1.68,63.11\n\n1.7,64.47\n\n1.73,66.28\n\n1.75,68.1\n\n1.78,69.92\n\n1.8,72.19\n\n1.83,74.46"
+    expected = 'y = 61.2721865421x + -39.0619559188'
+    actual = calculate_single_linear_regression(input_list).result
+    assert actual == expected
+
+def test_LinearRegression_EmptyInput_ReturnsError():
+    #preq-UNIT-TEST-6
+    input_list = ""
+    expected = 'Empty list. Format is one \"x,y\" pair per line.'
+    actual = str(calculate_single_linear_regression(input_list).error)
+    assert actual == expected
+
+def test_LinearRegression_NonNumeric_ReturnsError():
+    #preq-UNIT-TEST-6
+    input_list = "1.47,5.5\n1.56,6.65\n1.28,3.4\n1h8,10.2h"
+    expected = 'Invalid Format. Format is one \"x,y\" pair per line.'
+    actual = str(calculate_single_linear_regression(input_list).error)
+    assert actual == expected
+
+def test_LinearRegression_MultipleValues_ReturnsError():
+    #preq-UNIT-TEST-6
+    input_list = "1.47, 5.5\n1.56,6.65 3.6\n1.28,2 3.4\n1.8,10.2"
+    expected = 'Invalid Format. Format is one \"x,y\" pair per line.'
+    actual = str(calculate_single_linear_regression(input_list).error)
+    assert actual == expected
+
+def test_LinearRegression_SpacesAndCommasToIgnore_ReturnsFormula():
+    #preq-UNIT-TEST-6
+    input_list = "1.47, 5.5\n 1.56,6.65\n1.28, 3.4\n1.8,10.2 "
+    expected = 'y = 13.1394101877x + -13.6329490617'
+    actual = calculate_single_linear_regression(input_list).result
+    assert actual == expected
+
+def test_LinearRegression_OneCoordinatePair_ReturnsError():
+    #preq-UNIT-TEST-6
+    input_list = "1.5,3.6\n"
+    expected = 'One pair detected. Multiple pairs needed. Format is one \"x,y\" pair per line.'
+    actual = str(calculate_single_linear_regression(input_list).error)
+    assert actual == expected
+
+def test_PredictY_ValidInput_ReturnsPrediction():
+    #preq-UNIT-TEST-7
+    input_list = "1.535, 61.272186542107434, -39.061955918838656\n"
+    expected = 'y = 54.990850423296244'
+    actual = calculate_y_prediction(input_list).result
+    assert actual == expected
+
+def test_PredictY_EmptyInput_ReturnsError():
+    #preq-UNIT-TEST-7
+    input_list = ""
+    expected = 'Empty input. Format is \"x, m, b\" on one line.'
+    actual = str(calculate_y_prediction(input_list).error)
+    assert actual == expected
+
+def test_PredictY_MultipleLines_ReturnsError():
+    #preq-UNIT-TEST-7
+    input_list = "1.535, \n61.272186542107434, \n-39.061955918838656\n"
+    expected = 'Multiple lines detected. Format is \"x, m, b\" on one line.'
+    actual = str(calculate_y_prediction(input_list).error)
+    assert actual == expected
+
+def test_PredictY_NonNumeric_ReturnsError():
+    #preq-UNIT-TEST-7
+    input_list = "1.535, e61.272186542107434, -39.061955918838656"
+    expected = 'Non-Numbers detected. Format is \"x, m, b\" on one line.'
+    actual = str(calculate_y_prediction(input_list).error)
+    assert actual == expected
+
+def test_PredictY_MissingValues_ReturnsError():
+    #preq-UNIT-TEST-7
+    input_list = "1.535, 61.272186542107434"
+    expected = 'Missing Value(s). Format is \"x, m, b\" on one line.'
+    actual = str(calculate_y_prediction(input_list).error)
+    assert actual == expected
